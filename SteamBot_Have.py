ff69718025fc45b445b329a48c5fcab2ef74c428
Alison <alisonvieira.av29@gmail.com>
2020-07-08 15:49:18 -0300

    Get Link complete

--- SteamBot_Have.py
+++ SteamBot_Have.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from time import sleep
-from re import findall
+from re import findall,sub
 
-class SteamBotHave:
+class SteamBot_Have:
     
     def __init__ (self,url_steam):
         options = webdriver.ChromeOptions()
@@ -20,28 +20,34 @@ class SteamBotHave:
     def GetLink(self):
         sleep(1.5)
         doc = self.driver.page_source
-        regex="[A-zi]{5}://[A-zi]{5}.[A-zi]{12}.[A-zi]{3}/app/[0-9]([0-9]|)([0-9]|)([0-9]|)([0-9]|)([0-9]|)([0-9]|)([0-9]|)"
+        regex="htt.+store.+/app/[0-9]+"
         self.links = findall(rf"{regex}",doc)
 
     def CountLinks(self):
         print(len(self.links))
     
     def SaveLinks(self):
+        steamlink_have = open("steamlink_have.txt","w")
         
+        for link in self.links:
+            steamlink_have.write(link+"\n")
+            
+        steamlink_have.close()
     
     def CloseBrowser(self):
         self.driver.close()
     
 url_steam = "https://steamcommunity.com/id/ahsj4/games/?tab=all&sort=name"
-steam = SteamBotHave(url_steam)
+steam = SteamBot_Have(url_steam)
 steam.OpenBrowser()
 steam.GetLink()
 steam.CountLinks()
+steam.SaveLinks()
 steam.CloseBrowser()
 
-url_steam = "https://steamcommunity.com/id/yesy12/games/?tab=all&sort=name"
-steam2 = SteamBotHave(url_steam)
-steam2.OpenBrowser()
-steam2.GetLink()
-steam2.CountLinks()
-steam2.CloseBrowser()
+# url_steam = "https://steamcommunity.com/id/yesy12/games/?tab=all&sort=name"
+# steam2 = SteamBot_Have(url_steam)
+# steam2.OpenBrowser()
+# steam2.GetLink()
+# steam2.CountLinks()
+# steam2.CloseBrowser()
